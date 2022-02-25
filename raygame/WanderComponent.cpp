6443f5578109328e0bf72a2d0f1612d0f67d3a0f
Katelyn <katelynwest225@gmail.com>
2022-02-25 11:57:35 -0600

    Started on Wander component

--- raygame/WanderComponent.cpp
+++ raygame/WanderComponent.cpp
@@ -1,1 +1,25 @@
 #include "WanderComponent.h"
+#include "Transform2D.h"
+#include "Actor.h"
+#include <stdlib.h>
+
+WanderComponent::WanderComponent(float wanderRadius, const char* name) : Component::Component(name)
+{
+	m_wanderRadius = wanderRadius;
+}
+
+WanderComponent::WanderComponent(const char* name)
+{
+	MathLibrary::Vector2 ownerSize = getOwner()->getTransform()->getScale();
+	m_wanderRadius = ownerSize.x > ownerSize.y ? ownerSize.x : ownerSize.y;
+}
+
+MathLibrary::Vector2 WanderComponent::getIntendedDirection()
+{
+	MathLibrary::Vector2 direction = MathLibrary::Vector2()
+	return MathLibrary::Vector2();
+}
+
+void WanderComponent::update(float deltaTime)
+{
+}
--- raygame/WanderComponent.h
+++ raygame/WanderComponent.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "Component.h"
+#include "MoveComponent.h"
+
+class Transform2D;
+class SeekComponent;
+
 class WanderComponent :
 	public Component
 {
-private:
-
 public:
+	WanderComponent(float wanderRadius, const char* name = "WanderComponent");
+	WanderComponent(const char* name = "WanderComponent");
 
+	MathLibrary::Vector2 getIntendedDirection();
+
+	void update(float deltaTime) override;
+private:
+	float m_wanderRadius;
+	float m_wanderDistance;
+	MathLibrary::Vector2 m_intendedDirection;
+	SeekComponent* m_seekComponent;
 };
 
